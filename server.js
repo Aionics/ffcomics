7d25e842cf339ee6587213747ed3ddfc06b25ebb
aionics <aionics@list.ru>
2016-12-07 20:47:33 +0500

    test commit

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ app.set('json spaces', 40);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 app.use('/', express.static('./public'));
 app.get('/*', function (req, res, next) {
     res.sendFile("index.html", { root: __dirname + "/public"} )
 });
-
 
 const PORT = 8086;
 app.listen(PORT, function () {
