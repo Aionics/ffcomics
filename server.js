13ae65ed3d9ae307063b1e1baf6871bd0d1e2b8d
Kosarev Artem <Aionics@list.ru>
2016-12-09 16:45:32 +0500

    revert logs

--- server.js
+++ server.js
@@ -8,19 +8,12 @@ app.set('json spaces', 40);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use('/', express.static('./public'));
+app.use('/', express.static('./public'));
 app.get('/*', function (req, res, next) {
-    var ip = req.headers['x-forwarded-for'] ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress;
-    //This becouse i want your real ip c:
-    console.log('Got a connection from ' + ip + ' gonna show him this page:' + req.protocol + '://' + req.get('host') + req.originalUrl);
-    // res.sendFile("index.html", { root: __dirname + "/public" })
+    res.sendFile("index.html", { root: __dirname + "/public" })
 });
 
 const PORT = 8087;
 app.listen(PORT, function () {
-    console.log('123');
     console.log('listening on ' + PORT);
 });
