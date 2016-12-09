0833221cefa34b4d63c3ed20f75992eed610ee30
Kosarev Artem <Aionics@list.ru>
2016-12-09 16:14:42 +0500

    log

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ app.use(bodyParser.json());
 
 app.use('/', express.static('./public'));
 app.get('/*', function (req, res, next) {
-    res.sendFile("index.html", { root: __dirname + "/public"} )
+    var ip = req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    //This becouse i want your real ip c:
+    console.log('Got a connection from ' + ip + ' gonna show him this page:' + req.protocol + '://' + req.get('host') + req.originalUrl);
+    res.sendFile("index.html", { root: __dirname + "/public" })
 });
-
 
 const PORT = 8087;
 app.listen(PORT, function () {
