f619a20a0829bb171193f25b7e4b311fd4deaf3a
aionics <aionics@list.ru>
2016-12-11 06:34:13 +0500

    secure

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,10 +7,12 @@ const User = require('../models/User');
 const api = express();
 
 function checkIsAdmin(req, res, next) {
-    console.log('checking for admin');
     if (req.session.user_id) {
 
-        User.findById(req.session.user_id, function(err, user) {
+        User.findOne({
+            _id: mongoose.Types.ObjectId(req.session.user_id),
+            is_admin: true
+        }, function(err, user) {
             if (err) {
                 return res.send(err);
             }
@@ -18,7 +20,6 @@ function checkIsAdmin(req, res, next) {
             if (!user) {
                 return res.send('You are not an admin. If you are, try to relog');
             }
-            console.log('success');
             next();
         });
     } else {
@@ -33,9 +34,6 @@ api.post('/auth', function(req, res) {
     if (!login || !password) {
         return res.send('fill_required');
     }
-
-    console.log(req.session);
-
     User.findOne({
         login: login,
         password: User.hashPassword(password)
@@ -67,15 +65,12 @@ api.post('/logout', function(req, res) {
 });
 
 api.post('/createAdmin', checkIsAdmin, function(req, res) {
-    console.log('1');
     const NewUser = mongoose.model('User');
-    console.log(2);
     let user = {
         login: req.body.login,
         password: NewUser.hashPassword(req.body.password),
         is_admin: true
     }
-    console.log('trying create new user:', user);
     NewUser.create(user, function() {
         console.log('created user: ', user);
         res.send({
