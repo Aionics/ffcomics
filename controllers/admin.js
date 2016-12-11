8be6af7c8edf3f4886b9b0609f762c16398c4210
aionics <aionics@list.ru>
2016-12-11 05:37:01 +0500

    create user

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,7 +6,28 @@ const User = require('../models/User');
 
 const api = express();
 
-api.post('/auth', function (req, res) {
+function checkIsAdmin(req, res, next) {
+    if (req.session.user_id) {
+
+        User.findOne({
+            login: login,
+            password: User.hashPassword(password)
+        }, function(err, user) {
+            if (err) {
+                return res.send(err);
+            }
+
+            if (!user) {
+                return res.send('You are not an admin. If you are, try to relog');
+            }
+
+        });
+    } else {
+        return res.send('you need to log in');
+    }
+}
+
+api.post('/auth', function(req, res) {
     let login = req.body.login
     let password = req.body.password
 
@@ -38,5 +59,14 @@ api.post('/auth', function (req, res) {
         });
     });
 });
+
+api.post('/createAdmin', function(req, res) {
+    const User1 = mongoose.model('User');
+    User1.create({
+        login: 'Aionics',
+        password: User1.hashPassword('pizda'),
+        is_admin: true
+    }, function(){console.log('created');});
+})
 
 module.exports = api;
