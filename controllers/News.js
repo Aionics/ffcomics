56a091cb053ae3dadbb9ac00f30c3f1514a939e3
aionics <aionics@list.ru>
2017-01-01 22:25:32 +0500

    test multer

--- controllers/News.js
+++ controllers/News.js
@@ -8,14 +8,6 @@ const multer = require('multer')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'data/images/');
-    },
-    filename: function (req, file, cb) {
-        console.log(req);
-        let newName = transliterate(req.body.title).replace(' ', '_');
-        newName = escape(newName);
-        newName = newName + '-' + Date.now() + path.extname(file.originalname);
-        console.log('newName: ', newName)
-        cb(null, newName);
     }
 })
 
