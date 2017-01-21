2a2edf6c27c15304cc69a01382957463c7180881
aionics <aionics@list.ru>
2017-01-21 19:21:38 +0500

    middleware added

---
+++ middleware.js
@@ -0,0 +1,21 @@
+module.exports = {
+    checkIsAdmin: function () {
+        if (req.session.user_id) {
+            User.findOne({
+                _id: mongoose.Types.ObjectId(req.session.user_id),
+                is_admin: true
+            }, function (err, user) {
+                if (err) {
+                    return res.respond(err);
+                }
+
+                if (!user) {
+                    return res.respond('You are not an admin. If you are, try to relog');
+                }
+                next();
+            });
+        } else {
+            return res.respond('you need to log in');
+        }
+    }
+}
